22b20786f5ebd3ac9b18e7190611990e2ac7a72b
unknown <omidmollaei2002@gmail.com>
2023-06-04 13:03:43 +0430

    create a new file for rebuilding the model.

---
+++ model.py
@@ -0,0 +1,4 @@
+"""Implement an encoder-decoder network using attention mechanism
+to enhance its performance, building a neural machine translation
+model to translate persian sentences to english."""
+
