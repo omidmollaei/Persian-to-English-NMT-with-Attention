cd97a07038e6b1842a8b2f10dfb952a9a8815f6f
unknown <omidmollaei2002@gmail.com>
2023-06-06 19:09:58 +0430

    define custom masked accuracy metric

--- model.py
+++ model.py
@@ -182,3 +182,11 @@ def masked_loss(y_true, y_pred):
     mask = tf.cast(y_true != 0, loss.dtype)
     loss *= mask
     return tf.reduce_sum(loss)/tf.reduce_sum(mask)
+
+
+def masked_acc(y_true, y_pred):
+    y_pred = tf.argmax(y_pred, axis=-1)
+    y_pred = tf.cast(y_pred, y_true.dtype)
+    match = tf.cast(y_true == y_pred, tf.float32)
+    mask = tf.cast(y_true != 0, tf.float32)
+    return tf.reduce_sum(match) / tf.reduce_sum(mask)
